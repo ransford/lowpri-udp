79e9afe80b2dfc00b99d5da75ca974411e8656a1
Ben Ransford <ben@ransford.org>
2014-11-10 16:45:18 -0800

    fixed omg ugh

--- lowpriudp.sh
+++ lowpriudp.sh
@@ -16,41 +16,21 @@ tc qdisc del dev $IFACE root
 echo done flushing tc
 
 # create prio queueing discipline
+# http://lartc.org/howto/lartc.qdisc.classful.html
 #  root 1:prio
-#   /        \
-# 10:sfq   20:sfq
-tc qdisc replace dev $IFACE root handle 1: prio
-echo created root
-tc qdisc add dev $IFACE parent 1:1 handle 10: sfq
-echo created 1:10
-tc qdisc add dev $IFACE parent 1:2 handle 20: sfq
-echo created 1:20
+#      /   |   \
+# 10:sfq 20:sfq 30:sfq
+tc qdisc add dev $IFACE root handle 1: prio
 
-# udp to $UDPPORT is low-priority
-#echo adding filter for $UDPPORT/udp
-#tc filter add dev $IFACE protocol ip parent 1:0 prio 2 u32 \
-#	match ip protocol 17 0xff \
-#	match udp dst $UDPPORT 0xffff \
-#	classid 1:20
-#echo done adding filter
+# icmp: high priority
+tc filter add dev $IFACE protocol ip parent 1: prio 1 u32 match ip protocol 1 0xff flowid 1:1
 
-# clear existing iptables tomfoolery
-echo flushing iptables...
-iptables -t mangle -F
-echo done flushing iptables
+## tcp: high priority
+#tc filter add dev $IFACE protocol ip parent 1: prio 1 u32 match ip protocol 6 0xff flowid 1:1
 
-# UDP traffic to $UDPPORT is low priority
-iptables -t mangle -A POSTROUTING -o $IFACE -p udp --dport $UDPPORT \
-	-j CLASSIFY --set-class 1:20
+# udp blah: low priority
+tc filter add dev $IFACE protocol ip parent 1: prio 1 u32 match ip dport $UDPPORT 0xffff flowid 1:3
 
-# traffic with the min-delay flag is always high priority
-iptables -t mangle -A POSTROUTING -o $IFACE -p tcp \
-	-m tos --tos Minimize-Delay -j CLASSIFY --set-class 1:10
-
-
-# all TCP traffic gets high priority
-iptables -t mangle -A POSTROUTING -o $IFACE -p tcp -j CLASSIFY --set-class 1:10
-iptables -t mangle -A POSTROUTING -o $IFACE -p udp -j CLASSIFY --set-class 1:10
-echo iptables done
+echo tc filters created
 
 echo tc done
