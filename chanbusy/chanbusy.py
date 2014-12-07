f1c9a533763ecccbab8e5df62f4198024080b78c
Ben Ransford <ben@ransford.org>
2014-12-07 10:35:47 -0800

    click for cmdline processing

--- chanbusy/chanbusy.py
+++ chanbusy/chanbusy.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 
+import click
 import os, os.path
 import sys
 import time
 
-def main (samprate):
+@click.command()
+@click.argument('samprate_hz', type=int)
+def main (samprate_hz):
     def fp (p):
         return '/sys/kernel/debug/ieee80211/{}'.format(p)
 
-    freq = 1.0 / samprate
+    freq = 1.0 / samprate_hz
 
     try:
         path = '/sys/kernel/debug/ieee80211'
         phys = sorted([p for p in os.listdir(path)])
         phydirs = [fp(p) for p in phys if os.path.isdir(fp(p))]
     except OSError:
-        sys.stderr.write('cannot open debugfs path\n')
+        sys.stderr.write('cannot open debugfs path; exiting.\n')
         sys.exit(1)
 
     busy_pct_files = ['{}/ath9k/busy_pct'.format(d) for d in phydirs]
@@ -26,7 +29,4 @@ def main (samprate):
         time.sleep(freq)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        sys.stderr.write('Usage: chanbusy <sample_rate_in_Hz>\n')
-        sys.exit(1)
-    main(int(sys.argv[1]))
+    main()
